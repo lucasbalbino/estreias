74428baa65886bc2eb06817821ffca4a2312df9b
Lucas <lucasbmf@outlook.com>
2018-01-29 23:47:22 -0200

    Primeira versão das Estreias do Cinema

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+
+    $('.modal').on('hide.bs.modal', function(e) {
+        var $if = $(e.delegateTarget).find('iframe');
+        var src = $if.attr("src");
+        $if.attr("src", '/empty.html');
+        $if.attr("src", src);
+    });
+
+});
