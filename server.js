f3fea9f72821f1ec8a3c7e7987c23612f43f088d
Lucas <lucasbmf@outlook.com>
2018-03-12 21:30:52 -0300

    Adicionando conexão com banco

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const mysql = require('mysql');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+const connection = mysql.createConnection({
+    host: 'mysql472.umbler.com',
+    user: 'user-api',
+    password: 'g2eB#H2TOQ-',
+    port: 41890,
+    database: 'estreias_db'
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected!');
+});
+
 app.all('/*', function (req, res, next) {
     // CORS headers
-    res.header("Access-Control-Allow-Origin", "*"); // restrict it to the required domain
+    res.header('Access-Control-Allow-Origin', '*'); // restrict it to the required domain
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
 
     // Set custom headers for CORS
