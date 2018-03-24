b33266157e1c6a5f55270ac5e879a99c0906d70b
Lucas <lucasbmf@outlook.com>
2018-03-24 17:20:55 -0300

    Lidar com perda de conexão e outras alterações no package.json

--- server/db.js
+++ server/db.js
@@ -1,16 +1,37 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+const db_config = {
     host: 'mysql472.umbler.com',
     user: 'user-api',
     password: 'g2eB#H2TOQ-',
     port: 3306,
     database: 'estreias_db'
-});
+};
 
-connection.connect((err) => {
-    if (err) throw err;
-    console.log('Connected!');
-});
+
+function handleDisconnect() {
+    connection = mysql.createConnection(db_config); // Recreate the connection, since
+                                                    // the old one cannot be reused.
+
+    connection.connect(function(err) {              // The server is either down
+        if(err) {                                     // or restarting (takes a while sometimes).
+            console.log('DB ERROR: Error when connecting to db:', err);
+            setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+        }                                     // to avoid a hot loop, and to allow our node script to
+        console.log('DB Connected!');
+    });                                     // process asynchronous requests in the meantime.
+                                            // If you're also serving http, display a 503 error.
+    connection.on('error', function(err) {
+        console.log('db error', err);
+        if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+            handleDisconnect();                         // lost due to either server restart, or a
+        } else {                                      // connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
+
+handleDisconnect();
+
 
 module.exports = connection;
