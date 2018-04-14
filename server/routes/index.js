22f6f3af860447542312cc5a0ccaea72089b9499
Lucas <lucasbmf@outlook.com>
2018-04-14 17:06:26 -0300

    Forçando o https nas rotas

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,6 +7,17 @@ const list = require('./list');
 const dashboard = require('./dashboard');
 const form = require('./form');
 
+router.get('*', (req, res, next) => {
+    // checa se o header é HTTP ou HTTPS
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+        // faz o redirect para HTTPS
+        res.redirect("https://" + req.headers.host + req.url);
+    } else {
+        // segue com a sequência das rotas
+        next();
+    }
+});
+
 router.get('/api/dashboard', dashboard.getResumo);
 
 router.get('/api/estreias/:type/:date', estreias.getEstreias);
